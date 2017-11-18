19fb2cd8691b4052249b1ae0ef793c9a8bd8adec
Brad Caron <bacaron@imail.iu.edu>
2017-11-18 06:41:32 -0500

    initial commit

---
+++ rotateBvecs.m
@@ -0,0 +1,22 @@
+function out = rotateBvecs()
+
+switch getenv('ENV')
+case 'IUHPC'
+  disp('loading paths (HPC)')
+  addpath(genpath('/N/u/hayashis/BigRed2/git/vistasoft'))
+case 'VM'
+  disp('loading paths (VM)')
+  addpath(genpath('/usr/local/vistasoft'))
+end
+
+config = loadjson('config.json');
+
+% Set directories
+bvecs_pre = fullfile(config.bvecs);
+outAcpcTransform = load(fullfile('nodif_acpc.mat','-ASCII');
+outBvecs = fullfile('dwi.bvecs')
+
+% Rotate bvecs
+bvecs = dtiRawReorientBvecs(bvecs_pre,[],outAcpcTransform,outBvecs);
+exit;
+end
