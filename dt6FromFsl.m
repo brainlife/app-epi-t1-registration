1175188fae931ae25bbb3ba402bd1d0d206d7b9f
Brad Caron <bacaron@imail.iu.edu>
2017-11-23 04:41:47 -0500

    changes to dt6FromFsl to make dt6.json file

--- dt6FromFsl.m
+++ dt6FromFsl.m
@@ -11,16 +11,27 @@ case 'VM'
   addpath(genpath('/usr/local/jsonlab'))
 end
 
+config = loadjson('config.json');
+
 b0FileName = fullfile(pwd,'nodif_acpc_mean.nii.gz');
-t1FileName = fullfile(pwd,'t1.nii.gz');
+t1FileName = config.t1;
 outPathName = 'dt6';
 autoAlign = false;
 
 disp('creating dt6')
 dt6 = dtiMakeDt6FromFsl(b0FileName,t1FileName,outPathName,autoAlign);
 
+jsonStruct = [];
+jsonStruct.files.b0 = fullfile(pwd,'dwi_aligned_trilin_b0.nii.gz');
+jsonStruct.files.dt6 = dt6;
+jsonStruct.files.t1 = config.t1;
+jsonStruct.files.alignedDwRaw = fullfile(pwd,'dwi_aligned_trilin.nii.gz');
+jsonStruct.files.alignedDwBvecs = fullfile(pwd,'dwi_aligned_trilin.bvecs');
+jsonStruct.files.alignedDwBvals = fullfile(pwd,'dwi_aligned_trilin.bvals');
+
+
 disp('creating dt6.json')
-savejson('', dt6, 'dt6.json');
+savejson('', jsonStruct, 'dt6.json');
 
 exit;
 end
